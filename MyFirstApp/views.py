4962be7a6fd20942ecce0a7c0179ca7d881d84d2
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2024-12-28 07:16:50 +0800

    update views.py and liff.html for a better experience, I update view.py to contains the logic that gets liff.state parameter before the 'selected' URL parameter arrived and do thing with the liff.state parameter.update liff.html to use Django's way to set the pre-selected option of the flavor selector.

--- MyFirstApp/views.py
+++ MyFirstApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from urllib.parse import unquote
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -7,7 +8,12 @@ def HomePage(request):
     viewerName="Hananiah"
     response = render(request, "HomePage.html", locals())
     return response
+
+    
 def liffPage(request):
     selected = request.GET.get('selected','')
+    if selected == '':
+        state = request.GET.get('liff.state','')
+        selected = unquote(state[state.index('=')+1:])
     response = render(request, "liff.html", locals())
     return response
