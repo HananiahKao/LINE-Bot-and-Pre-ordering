341a31139b919c3c75f3ee4df97518f4bff7114e
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-01-07 20:37:20 +0800

    use user_id instead of userId. Test.

--- MyFirstApp/lineCallBack.py
+++ MyFirstApp/lineCallBack.py
@@ -29,5 +29,5 @@ def callBack(request):
 def handle_message(event):
     if event.message.text.endswith('tapping'):
         message_to_send=createMessage()
-        line_bot_api.push_message(event.source.userId,message_to_send)
+        line_bot_api.push_message(event.source.user_id,message_to_send)
 
