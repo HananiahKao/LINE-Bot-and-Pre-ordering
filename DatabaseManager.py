631e8eb75c7b05a28c332a0bb4722309ebd39596
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-01-12 08:05:51 +0800

    add functionality to delete existing key.

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -5,7 +5,7 @@ import os
 class DBManager:
     dbpath = Path(__file__).resolve().parent/'database'/'database.txt'
     def write_value(self,key,value):
-        if not self.dbpath.read_text() == '' and not key == '' and not self.keyExists(key):
+        if not self.dbpath.read_text() == '' and not key == '' and not self.key_exists(key):
             content = self.dbpath.read_text()
             if content.endswith('\n'):
                 newlineOrNothing = ''
@@ -14,7 +14,7 @@ class DBManager:
             self.dbpath.write_text(f'{content}{newlineOrNothing}{key}:{value}')
         elif self.dbpath.read_text() == '':
             self.dbpath.write_text('{}:{}'.format(key,value))
-        elif self.keyExists(key):
+        elif self.key_exists(key):
             file_content = self.dbpath.read_text()
             lines = file_content.splitlines(keepends=True)
             index = 0
@@ -41,18 +41,39 @@ class DBManager:
             return False
 
     def read_value(self,key,default):
-        if self.keyExists(key):
+        if self.key_exists(key):
             file_content = self.dbpath.read_text()
             lines = file_content.splitlines(keepends=True)
             index = 0
-            lineOfKey = ''
-            for line in lines:
-                if line.startswith(f'{key}:'):
-                    lineOfKey = line
-                    break
+            lineOfKey = self.find_key(key)
             if not lineOfKey == '':
                 return lineOfKey[lineOfKey.index(':')+1:].replace('\n','')
             else:
                 return default
         else:
             return default
+
+
+    def find_key(self,key):
+        file_content = self.dbpath.read_text()
+        lines = file_content.splitlines(keepends=True)
+        index = 0
+        lineOfKey = ''
+        for line in lines:
+            if line.startswith(f'{key}:'):
+                return line
+
+    def delete_key(self,key):
+        file_content = self.dbpath.read_text()
+        lines = file_content.splitlines(keepends=True)
+        index = 0
+        lineOfKey = self.find_key(key)
+        for line in lines:
+            if line.startswith(f'{key}:'):
+                lines[index] = ''
+                break
+            index = index + 1
+        self.dbpath.write_text("".join(lines))
+
+
+
