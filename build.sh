0efb36c14df4347321ce98ae112cdc30132d08e3
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-01-19 13:21:27 +0800

    use the third way to install HomeBrew.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-git clone https://github.com/Homebrew/brew homebrew
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip-components 1 -C homebrew
 eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
