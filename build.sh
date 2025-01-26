492cb09c826cf1e9efb22aed7a6655b4758eb353
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-01-26 12:17:13 +0800

    Update build.sh, comment out code that use HomeBrew.

--- build.sh
+++ build.sh
@@ -3,22 +3,22 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-git clone https://github.com/Homebrew/brew homebrew
-eval "$(homebrew/bin/brew shellenv)"
-brew update --force
-chmod -R go-w "$(brew --prefix)/share/zsh"
+#git clone https://github.com/Homebrew/brew homebrew
+#eval "$(homebrew/bin/brew shellenv)"
+#brew update --force
+#chmod -R go-w "$(brew --prefix)/share/zsh"
 
-brew upgrade
-brew install python@3.13 --verbose
-brew update
-brew install watchman
-watchman watch ./database/
-watchman -j <<-EOT
-["trigger", "./database", {
-  "name": "test",
-  "command": ["./commitDB.sh"]
-}]
-EOT
+#brew upgrade
+#brew install python@3.13 --verbose
+#brew update
+#brew install watchman
+#watchman watch ./database/
+#watchman -j <<-EOT
+#["trigger", "./database", {
+#  "name": "test",
+ # "command": ["./commitDB.sh"]
+#}]
+#EOT
 pip install -r requirements.txt
 
 # Convert static asset files
