043c903669ca6857e676b33f3efdc66c736200f6
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-02-05 19:49:51 +0800

    remove the lines that install python@3.13 and watchman, because that I have succesed use subproccess and Shell Script to commit database.Remove the lines that have 'git config' because that have been included in the database/commitDB.sh file.

--- build.sh
+++ build.sh
@@ -7,21 +7,7 @@ eval "$(homebrew/bin/brew shellenv)"
 echo $(homebrew/bin/brew shellenv)>>~/.bash_profile
 brew update --force
 chmod -R go-w "$(brew --prefix)/share/zsh"
-
-#brew upgrade
-#brew install python@3.13 --verbose
-#brew update
-#brew install watchman
-#watchman watch ./database/
-#watchman -j <<-EOT
-#["trigger", "./database", {
-#  "name": "test",
- # "command": ["./commitDB.sh"]
-#}]
-#EOT
 git clone https://HananiahKao:$GITHUB_ACCESS_TOKEN@github.com/HananiahKao/Database.git database
-git config --global user.email "HananiahKao@users.noreply.github.com"
-git config --global user.name "HananiahKao"
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
