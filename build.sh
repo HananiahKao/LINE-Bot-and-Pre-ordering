fb728b00636b66697faf77cd2da4e055c7b2f3d9
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-04-15 06:22:14 -0400

    Update build.sh, cancel clone of HomeBrew

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-
-git clone https://github.com/Homebrew/brew homebrew
-eval "$(homebrew/bin/brew shellenv)"
-echo $(homebrew/bin/brew shellenv)>>~/.bash_profile
-brew update --force
-chmod -R go-w "$(brew --prefix)/share/zsh"
 git clone https://HananiahKao:$GITHUB_ACCESS_TOKEN@github.com/HananiahKao/Database.git database
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
